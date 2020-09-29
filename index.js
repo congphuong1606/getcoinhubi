419aaa39b72369db3ceb52fbc09b22a5e471b7d6
congphuong1606 <cphuong.coldboy@gmail.com>
2020-09-30 00:32:50 +0700

    sdfsdf

--- index.js
+++ index.js
@@ -17,10 +17,8 @@ function myTimer() {
           method:'get',
           url:'https://api-cloud.huobi.co.jp/market/detail/merged?symbol=btcjpy'
         }).then(function (response) {
-          console.log(response.data);
             axios.get("https://script.google.com/macros/s/AKfycbwhq4DLdwrfkow57q2LaklmujQSWvTw3ztxYtyj6zUt5jN2tprd/exec?action=huobijp&price="+response.data.tick.close)
             .then((response)  =>  {
-              console.log(response.data);
               isdone=true;
             }, (error)  =>  {isdone=true;})  
         })
@@ -33,9 +31,6 @@ function myTimer() {
   }
 }
 setInterval(myTimer, 6000);
-
-
-
 
 app.get('/', function(req, res) {
   res.send('HELLO');
