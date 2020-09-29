faeadbada34d2f8259e7e039b521d0b8a0edf1a2
congphuong1606 <cphuong.coldboy@gmail.com>
2020-09-29 23:36:43 +0700

    Sfsdfsd

--- index.js
+++ index.js
@@ -5,6 +5,24 @@ var app = express()
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
+
+
+
+
+
+function myTimer() {
+  axios.get("https://script.google.com/macros/s/AKfycbxuaXtlqR-Gvr8yEejWTZc18FwpinN703OIiaPOZRT-CwFTwBU/exec?action=insert-data&name=phuong&phone=0965100635&address=nghikieu,nghiloc,nghean")
+     .then((response)  =>  {
+       console.log(response.data);
+     }, (error)  =>  {
+       
+ })
+
+   }
+ setInterval(myTimer, 6000);
+
+
+
 app.get('/', function(req, res) {
   axios({
       method:'get',
